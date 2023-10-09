7f69f9a2a5949ff78ce19d531b4d82df82a206da
sunil <sunilporal713@gmail.com>
2023-10-09 20:11:12 +0530

    shell-laeaning

---
+++ demo.sh
@@ -0,0 +1,12 @@
+#list the files
+ls
+# create a file
+touch file1
+cat file1
+mkdir -p poral
+cd poral
+
+ls
+
+
+
---
+++ variable.sh
@@ -0,0 +1,8 @@
+Trainer=sunil
+class=Devops
+
+echo trainer name -$Trainer
+echo class name -${class}
+
+date=$(date +%F)
+echo Hi today date is $date
